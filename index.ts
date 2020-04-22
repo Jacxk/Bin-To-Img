61e9dfa6545d045f0c787857bd79ded1129c6d3a
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2020-04-22 23:54:21 +0200

    Made the code gets colored (#1)
    * made it colored!
    
    * made it colored
    
    * removed the bad </script> tag
    
    * removed zombie code
    changed the import
    made highlighted content a variable
    
    * changed toFile to toBuffer, my bad
    
    * Changed tabs to spaces
    
    * Refactored line from last commit
    
    Co-authored-by: Yanluis Fermin <tfermin1827@hotmail.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as SourceBin from 'sourcebin-wrapper';
 import { readFileSync } from 'fs';
 import * as pdf from 'html-pdf';
+import { highlight } from 'highlight.js';
 
 const [ b, th ] = process.argv.slice(2);
 
@@ -10,30 +11,13 @@ export function convert(bin: string, theme?: string) {
             `./presets/${ theme || th || "default" }.html`,
             { encoding: "utf8" }
         ));
+		
         const { files } = await SourceBin.get(bin);
         const [ file ] = files;
-        const content = file.content
-            .replace(/^ {4}/gm, "  ")
-            .replace(/\r/g, '')
-            .split(/^/gms)
-            .map((val, i, array) => {
-                return `<line><num>${ i + 1 }</num><span>${
-                    val.replace(/</g, "&lt;")
-                        .replace(/</g, "&gt;")
-                }</span></line>`
-            })
-            .map((val, i, array) => {
-                const [ , maxLine ] = array[array.length - 1]
-                    .match(/<num>(\d+)<\/num>/) || [ undefined, '0' ]
-                const lineNumber = String(i + 1);
-                const line = lineNumber.padStart(maxLine.length, " ")
-                return val.replace(
-                    new RegExp(`<num>${ lineNumber }</num>`, "g"),
-                    `<num>${ line }</num>`
-                )
-            })
-            .join('')
-        const html = preset.replace(/{{ contents }}/, content)
+        const content = file.content;
+		
+        const parsed = highlight(file.language.aceMode, content).value;
+        const html = preset.replace(/{{ contents }}/, parsed);
 
         pdf.create(html, {
             type: "png",
