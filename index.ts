d50f4171a67fa19a60b88ee463cddeda8a9dc983
ZackGabri <mrkoko134@gmail.com>
2020-04-22 17:19:58 +0200

    removed zombie code changed the import made highlighted content a variable

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as SourceBin from 'sourcebin-wrapper';
 import { readFileSync } from 'fs';
 import * as pdf from 'html-pdf';
-import * as hljs from 'highlight.js';
+import { highlight } from 'highlight.js';
 
 const [ b, th ] = process.argv.slice(2);
 
@@ -11,37 +11,20 @@ export function convert(bin: string, theme?: string) {
             `./presets/${ theme || th || "default" }.html`,
             { encoding: "utf8" }
         ));
+		
         const { files } = await SourceBin.get(bin);
         const [ file ] = files;
-        const content = file.content
-            // .replace(/^ {4}/gm, "  ")
-            // .replace(/\r/g, '')
-            // .split(/^/gms)
-            // .map((val, i, array) => {
-            //     return `<line><num>${ i + 1 }</num><span>${
-            //         val.replace(/</g, "&lt;")
-            //             .replace(/</g, "&gt;")
-            //     }</span></line>`
-            // })
-            // .map((val, i, array) => {
-            //     const [ , maxLine ] = array[array.length - 1]
-            //         .match(/<num>(\d+)<\/num>/) || [ undefined, '0' ]
-            //     const lineNumber = String(i + 1);
-            //     const line = lineNumber.padStart(maxLine.length, " ")
-            //     return val.replace(
-            //         new RegExp(`<num>${ lineNumber }</num>`, "g"),
-            //         `<num>${ line }</num>`
-            //     )
-            // })
-            // .join('')
-        const html = preset.replace(/{{ contents }}/, hljs.highlight(file.language.aceMode, content).value);
+        const content = file.content;
+		
+		const parsed = highlight(file.language.aceMode, content).value;
+        const html = preset.replace(/{{ contents }}/, parsed);
 
         pdf.create(html, {
             type: "png",
             format: "Letter",
             orientation: "portrait",
             renderDelay: 0,
-        }).toBuffer(function (err, buffer) {
+        }).toFile("filePath.png", function (err, buffer) {
             if (err) return reject(err);
             resolve(buffer)
         });
