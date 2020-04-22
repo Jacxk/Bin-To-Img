f6281c43a10365e6a271a876b23b87ec348493b6
ZackGabri <mrkoko134@gmail.com>
2020-04-22 02:07:40 +0200

    made it colored!

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as SourceBin from 'sourcebin-wrapper';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import * as pdf from 'html-pdf';
+import * as hljs from 'highlight.js';
 
 const [ b, th ] = process.argv.slice(2);
 
@@ -13,27 +14,27 @@ export function convert(bin: string, theme?: string) {
         const { files } = await SourceBin.get(bin);
         const [ file ] = files;
         const content = file.content
-            .replace(/^ {4}/gm, "  ")
-            .replace(/\r/g, '')
-            .split(/^/gms)
-            .map((val, i, array) => {
-                return `<line><num>${ i + 1 }</num><span>${
-                    val.replace(/</g, "&lt;")
-                        .replace(/</g, "&gt;")
-                }</span></line>`
-            })
-            .map((val, i, array) => {
-                const [ , maxLine ] = array[array.length - 1]
-                    .match(/<num>(\d+)<\/num>/) || [ undefined, '0' ]
-                const lineNumber = String(i + 1);
-                const line = lineNumber.padStart(maxLine.length, " ")
-                return val.replace(
-                    new RegExp(`<num>${ lineNumber }</num>`, "g"),
-                    `<num>${ line }</num>`
-                )
-            })
-            .join('')
-        const html = preset.replace(/{{ contents }}/, content)
+            // .replace(/^ {4}/gm, "  ")
+            // .replace(/\r/g, '')
+            // .split(/^/gms)
+            // .map((val, i, array) => {
+            //     return `<line><num>${ i + 1 }</num><span>${
+            //         val.replace(/</g, "&lt;")
+            //             .replace(/</g, "&gt;")
+            //     }</span></line>`
+            // })
+            // .map((val, i, array) => {
+            //     const [ , maxLine ] = array[array.length - 1]
+            //         .match(/<num>(\d+)<\/num>/) || [ undefined, '0' ]
+            //     const lineNumber = String(i + 1);
+            //     const line = lineNumber.padStart(maxLine.length, " ")
+            //     return val.replace(
+            //         new RegExp(`<num>${ lineNumber }</num>`, "g"),
+            //         `<num>${ line }</num>`
+            //     )
+            // })
+            // .join('')
+        const html = preset.replace(/{{ contents }}/, hljs.highlight(file.language.aceMode, content).value);
 
         pdf.create(html, {
             type: "png",
